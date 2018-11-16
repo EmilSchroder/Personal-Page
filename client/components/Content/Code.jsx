d56e6e14de40eb1fa3b39e2312c4c3e8371559ad
EmilSchroder <eschro66@gmail.com>
2018-11-16 14:24:27 +1300

    Removing some full stops

--- client/components/Content/Code.jsx
+++ client/components/Content/Code.jsx
@@ -4,7 +4,7 @@ const Code = () => {
     return(
         <React.Fragment>
             <h1>Cyberman</h1>
-            <p className='bodytext'><strong>Because we can't have fictional superpowers we must settle for real ones.</strong></p>
+            <p className='bodytext'><strong>Because we can't have fictional superpowers we must settle for real ones</strong></p>
             <p className='bodytext'>Fullstack that isn't a house of cards</p>
             <p className='bodytext'>Looking to try out anything new</p>
             <p className='bodytext'>Today's code will be better than yesterday's</p>

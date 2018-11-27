8c7d3712961e657f6d0b7b193f807f3de1091fde
EmilSchroder <eschro66@gmail.com>
2018-11-27 17:05:48 +1300

    redesign design coming together

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import {HashRouter as Router, Route} from 'react-router-dom'
+
+import Text from './Text'
 
 
-import EmilComp from './EmilComp'
-import Nav from './Nav'
-import Content from './Content/index'
 
 
 class App extends React.Component{
@@ -12,46 +10,19 @@ class App extends React.Component{
     constructor(props){
         super(props)
 
-        this.state = {
-            windowWidth: window.innerWidth,
-        }
 
-        this.widthChange = this.widthChange.bind(this)
-        this.renderLaptop = this.renderLaptop.bind(this)
     }
 
-    componentDidMount(){
-        addEventListener('resize', this.widthChange)
-    }
-
-
-    widthChange(){
-        this.setState({
-            windowWidth: window.innerWidth
-        })
-    }
-
-    renderLaptop(){
-        if (this.state.windowWidth>'768'){
-            return(
-                <React.Fragment>
-                    <Content />
-                    <Nav />
-                </React.Fragment>
-            )
-        }
-    }
 
 
 render(){
     return (
         
-        <Router>
             <div id='wrapper'>
-                <EmilComp />
-                {this.renderLaptop()}
+                <Text />
+                <div id='slider'><input type='range' min='0' max='1' value='1' /></div>
+                
             </div>
-        </Router>
     )
 
 }
